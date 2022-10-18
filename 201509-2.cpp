a3243e7fbc2b79208b0340c6678aa480e0fc0c00
Ricardo <2528042945@qq.com>
2022-10-18 17:53:20 +0800

    日期计算

---
+++ 201509-2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using gg = long long;
+
+int main() {
+	int y, d, i;
+	cin >> y >> d;
+	int m[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	if ((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0)) {
+		m[1] = 30;
+	}
+	for (i = 0; i < 12; i++) {
+		if (d > m[i])
+			d = d - m[i];
+		else
+			break;
+	}
+	cout << i + 1 << '\n' << d << endl;
+	return 0;
+}
